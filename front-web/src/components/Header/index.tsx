353a8405da67eb449353255c838eb8bbf3a27931
cesarionto <cesariopereiraneto@gmail.com>
2020-09-16 21:36:38 -0300

    Criando pagina Home

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles.css'
-import {ReactComponent as Logo} from '../../assets/Logo.svg'
+import {ReactComponent as Logo} from '../../assets/logo.svg'
 
 const Header = () => (
     <header className="main-header">
