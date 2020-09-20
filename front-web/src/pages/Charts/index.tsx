81738f7dc78d25b2eb426a7f26cadb2bbdbc4f17
cesarionto <cesariopereiraneto@gmail.com>
2020-09-20 16:56:22 -0300

    Inserindo HTTPS

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = "http://sds1-cesario.herokuapp.com"
+const BASE_URL = "https://sds1-cesario.herokuapp.com"
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([])
